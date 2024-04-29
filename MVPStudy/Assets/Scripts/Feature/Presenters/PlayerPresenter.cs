c2c92a749f94cf8f4989d11097c697fc445f6ed7
Hayato222 <g2227070@st.t-kougei.ac.jp>
2024-04-29 19:29:28 +0900

    Implementing the InputSystem

--- MVPStudy/Assets/Scripts/Feature/Presenters/PlayerPresenter.cs
+++ MVPStudy/Assets/Scripts/Feature/Presenters/PlayerPresenter.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using Feature.Models;
 using Feature.Views;
+
+
 namespace Feature.Presenters
 {
     /// <summary>
     /// 外部からの入力を受け取りModelに通知、viewに反映
     /// </summary>
-    public class PlayerPresenter
+    public class PlayerPresenter:MonoBehaviour
     {
         
     }
--- MVPStudy/Assets/Scripts/Feature/Views/PlayerView.cs
+++ MVPStudy/Assets/Scripts/Feature/Views/PlayerView.cs
@@ -1,12 +1,54 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
 namespace Feature.Views
 {
     /// <summary>
     /// playerのview
     /// 具体的な操作はPresenterに任せる（ここでは何もしない）
     /// </summary>
-    public class PlayerView
+    public class PlayerView : MonoBehaviour
     {
-        
+        private Rigidbody rb;
+        public float movementSpeed = 5f;
+        private bool moving;
+        private float horizontal;
+        private float vertical;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        public void Update()
+        {
+            if (moving)
+            {
+                var movement = new Vector3(horizontal, 0f, vertical).normalized * movementSpeed;
+                rb.AddForce(movement,ForceMode.VelocityChange);
+            }
+            else
+            {
+                
+            }
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            var movementInput = context.ReadValue<Vector2>();
+            horizontal = movementInput.x;
+            vertical = movementInput.y;
+
+            Debug.Log("Horizontal: " + horizontal + ", Vertical: " + vertical);
+
+            if (context.started)
+            {
+                moving = true;
+            }
+            else if (context.canceled)
+            {
+                moving = false;
+            }
+        }
     }
 }
