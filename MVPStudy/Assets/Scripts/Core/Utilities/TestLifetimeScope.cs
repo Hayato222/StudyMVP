ce72cccf3ac2344cadc250870f7c8dca4e1f1e6a
Hayato222 <g2227070@st.t-kougei.ac.jp>
2024-05-13 16:44:36 +0900

    Add TestMonoBehaviourScript

---
+++ MVPStudy/Assets/Scripts/Core/Utilities/TestLifetimeScope.cs
@@ -0,0 +1,20 @@
+using System;
+using Core.Utilities;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+using Logger = Core.Utilities.Logger;
+
+public class TestLifetimeScope : LifetimeScope
+{
+    [SerializeField] private TestMonoBehaviour testMonoBehaviour;
+    
+    protected override void Configure(IContainerBuilder builder)
+    {
+        builder.Register<Logger>(Lifetime.Singleton);
+        builder.Register<Calulator>(Lifetime.Singleton);
+        builder.Register<HogeClass>(Lifetime.Singleton);
+
+        builder.RegisterComponent(testMonoBehaviour);
+    }
+}
---
+++ MVPStudy/Assets/Scripts/Core/Utilities/TestMonoBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using VContainer;
+
+namespace Core.Utilities
+{
+    public class TestMonoBehaviour : MonoBehaviour
+    {
+        private HogeClass hogeClass;
+
+        [Inject]
+        public void Inject(HogeClass hogeClass)
+        {
+            this.hogeClass = hogeClass;
+        }
+
+        public void Start()
+        {
+            hogeClass.CalulatorTest(2,3);
+        }
+    }
+}
